c87bb0f71626b77eca09830cb8ef095fb0950229
Matt Thompson <matt.thompson@johnlewis.co.uk>
2022-03-05 22:57:16 +0000

    Add game activity and simple generate name listener event

--- app/src/main/java/com/birdle/MainActivity.kt
+++ app/src/main/java/com/birdle/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.birdle
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.birdle.R.layout.activity_main
 
-class BirdleActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     private var message: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class BirdleActivity : AppCompatActivity() {
     }
 
     private fun tapBirdle() {
-        val birdName = generateBirdle(birdNames)
-
-        message!!.text = String.format("Birdle wordle is %s", birdName)
+        val gameIntent = Intent(this, GameActivity::class.java)
+        startActivity(gameIntent)
     }
 }
